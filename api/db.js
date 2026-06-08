e6805e294daa4909e43ac6470cb8cc596f29ee97
Custodio1984 <daamm@msn.com>
2026-06-08 17:52:31 -0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,1 +1,14 @@
 
+import { Client } from 'pg';
+
+export async function query(sql, params) {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  await client.connect();
+  const result = await client.query(sql, params);
+  await client.end();
+
+  return result;
+}
